1f7955176f401396c254be5f24db534d33dd4897
Saad <charbelsaadghassibe@outlook.es>
2024-06-15 20:34:46 -0500

    departments toast

--- src/app/views/departments/create/create.component.ts
+++ src/app/views/departments/create/create.component.ts
@@ -12,7 +12,14 @@ import {
   FormLabelDirective,
   FormSelectDirective,
   RowComponent,
+  ToastBodyComponent,
+    ToastComponent,
+    ToastHeaderComponent,
+    ToasterComponent,
   TextColorDirective,
+  ProgressBarComponent,
+  ProgressBarDirective,
+  ProgressComponent
 } from '@coreui/angular';
 import { IconDirective } from '@coreui/icons-angular';
 import { FormsModule } from '@angular/forms';
@@ -33,12 +40,30 @@ import { FormsModule } from '@angular/forms';
     TextColorDirective,
     FormSelectDirective,
     FormsModule,
+    ToastBodyComponent,
+    ToastComponent,
+    ToastHeaderComponent,
+    ToasterComponent,
+    ProgressBarComponent,
+  ProgressBarDirective,
+  ProgressComponent,
+    
   ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
 export class CreateComponent {
   name = '';
+  email = "";
+  password = "";
+  birthdate = "";
+  departmentId = 0;
+  role = ""; 
+  position = 'top-end';
+  visible = false;
+  percentage = 0;
+  toastMessage = ''; 
+  toastClass: string = ''; 
 
   constructor(
     private createDepartmentService: CreateDepartmentService,
@@ -47,12 +72,38 @@ export class CreateComponent {
 
   createDepartment(): void {
     this.createDepartmentService.postDepartment({ name: this.name }).subscribe({
-      next: () => {
-        this.router.navigate(['/departments']);
+      next: (response) => {
+        this.toggleToast('Departamento creado exitosamente', true); // Mostrar toast de éxito
+        setTimeout(() => {
+          this.router.navigate([`departments`]); 
+        },1500)
       },
       error: (error) => {
+        this.toggleToast('Error al crear departamento', false); 
         console.log(error);
       },
     });
   }
+
+  toggleToast(message: string, success: boolean): void {
+    this.visible = true;
+    this.percentage = 100;
+    if (success) {
+      this.toastMessage = message;
+      this.toastClass = 'toast-success';
+    } else {
+      this.toastMessage = message;
+      this.toastClass = 'toast-error';
+    }
+  }
+
+  onVisibleChange($event: boolean) {
+    this.visible = $event;
+    this.percentage = !this.visible ? 0 : this.percentage;
+  }
+
+  onTimerChange($event: number) {
+    this.percentage = $event * 100;
+  }
+
 }
