5a191f33cc94149548e34c78a825de0e328822bb
GreenieWarren <87400557+GreenieWarren@users.noreply.github.com>
2024-07-14 02:48:57 -0400

    Diseño del hmtl de settings

--- src/app/views/settings/settings.component.ts
+++ src/app/views/settings/settings.component.ts
@@ -1,12 +1,104 @@
 import { Component } from '@angular/core';
+import { WidgetsDropdownComponent } from '../widgets/widgets-dropdown/widgets-dropdown.component';
+import {
+  AvatarComponent,
+  ButtonCloseDirective,
+  ButtonDirective,
+  ButtonGroupComponent,
+  CardBodyComponent,
+  CardComponent,
+  CardFooterComponent,
+  CardHeaderComponent,
+  ColComponent,
+  FormCheckLabelDirective,
+  GutterDirective,
+  ModalModule,
+  ModalTitleDirective,
+  PageItemDirective,
+  PageLinkDirective,
+  PaginationComponent,
+  ProgressBarComponent,
+  ProgressBarDirective,
+  ProgressComponent,
+  RowComponent,
+  TableDirective,
+  TextColorDirective,
+  ThemeDirective,
+  ToastBodyComponent,
+  ToastComponent,
+  ToastHeaderComponent,
+  ToasterComponent,
+} from '@coreui/angular';
+import { IconDirective } from '@coreui/icons-angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChartjsComponent } from '@coreui/angular-chartjs';
+import { NgIf, NgStyle } from '@angular/common';
+import { WidgetsBrandComponent } from '../widgets/widgets-brand/widgets-brand.component';
+import { RouterLink } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
-  standalone: true,
-  imports: [],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
+  standalone: true,
+  imports: [
+    WidgetsDropdownComponent,
+    TextColorDirective,
+    CardComponent,
+    CardBodyComponent,
+    RowComponent,
+    ColComponent,
+    ButtonDirective,
+    IconDirective,
+    ReactiveFormsModule,
+    ButtonGroupComponent,
+    FormCheckLabelDirective,
+    ChartjsComponent,
+    NgStyle,
+    CardFooterComponent,
+    GutterDirective,
+    ProgressBarDirective,
+    ProgressComponent,
+    WidgetsBrandComponent,
+    CardHeaderComponent,
+    TableDirective,
+    AvatarComponent,
+    ModalTitleDirective,
+    ThemeDirective,
+    ButtonCloseDirective,
+    PageItemDirective,
+    PageLinkDirective,
+    PaginationComponent,
+    RouterLink,
+    ToastBodyComponent,
+    ToastComponent,
+    ToastHeaderComponent,
+    ToasterComponent,
+    ModalModule,
+    ProgressBarComponent,
+    NgIf,
+    NgxSpinnerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule,
+  ],
 })
 export class SettingsComponent {
+  isSubscribedToEmailsMessage: string = 'Testing';
+  buttonIsOn: boolean = false;
 
+  printStuff(answer: boolean): void {
+    this.buttonIsOn = !answer;
+    console.log(this.buttonIsOn);
+  }
 }
